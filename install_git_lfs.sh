9cbd9ce57cb471593ec6e704c42d99f8c2b5c21a
Tom Zhang <tomzhang32@users.noreply.github.com>
2017-02-16 09:36:09 -0800

    Change git co to git checkout

--- install_git_lfs.sh
+++ install_git_lfs.sh
@@ -5,7 +5,7 @@ rm -rf $DIR
 mkdir -p $DIR
 git clone git@github.com:git-lfs/git-lfs ${DIR}/git-lfs
 cd ${DIR}/git-lfs
-git co release-release-1.5-backport-1737
+git checkout release-release-1.5-backport-1737
 go install
 git-lfs version
 git lfs install
